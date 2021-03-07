f1ce7f8430f5a31b55e6b79fda32ae39ba611b67
Florian Redl <florian.redl94.gmail.com>
2021-03-07 18:14:54 +0100

    Projektdateien hinzufügen.

---
+++ FLRUpdater/Updater.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLRUpdater
+{
+    public class Updater
+    {
+
+        /// <summary>
+        /// Logo to use for all Windows
+        /// </summary>
+        public static string Logo { get; set; }
+
+
+        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xml"></param>
+        public void OnStartUp(string xml)
+        {
+            //Check ob XML vorhanden
+            //Check ob update benötigt
+
+            if (true)
+            {
+                StartUpWindow startUpWindow = new StartUpWindow();
+                startUpWindow.Show();
+            }
+            
+
+            //Öffnet fenster
+
+            //sucht nach Updates
+
+            
+
+        }
+
+        public void OnRuntime()
+        {
+            
+            //Sucht nach Updates
+
+            //wenn verfügbar updaten und neu starten 
+        }
+    }
+}
